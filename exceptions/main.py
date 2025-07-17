19f30ad21ce1e26b143de0b95d9aa9cfddaa8e22
Colin Bob <colinfrankb@gmail.com>
2025-07-17 22:13:18 +0200

    exception handling notes

--- exceptions/main.py
+++ exceptions/main.py
@@ -5,6 +5,7 @@ import traceback
 def raise_exception_without_try_block():
     print("Before exception")
     raise Exception("does this stop execution?")
+    # it does stop execution, the process closes
     exc_type, exc_value, exc_tb = sys.exc_info()
     traceback.print_exception(exc_type, exc_value, exc_tb) 
     print("After exception")
@@ -14,7 +15,7 @@ def raise_exception_with_try_block():
         raise Exception("raised exception in try block")
     except Exception as err:
         print("Exception caught")
-
+        # sys.exc_info is only available inside the except block
         exc_type, exc_value, exc_tb = sys.exc_info()
         print(exc_type, exc_value, exc_tb)
 
